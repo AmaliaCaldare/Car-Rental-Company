74e72ecd48ee61a783caf911e81d5a767e2b6373
Plamena <plam@html24.net>
2021-04-30 11:21:38 +0200

    add delete user

--- api/user.js
+++ api/user.js
@@ -21,7 +21,7 @@ router.get('/user/:id', (req, res) => {
         })
 })
 
-  router.post('/add-user', (req, res) => {
+  router.post('/user/add', (req, res) => {
     const { firstName, lastName, phoneNumber, email, licenceNum, passportNum,password,addressId } = req.body;
     if(firstName && lastName &&  phoneNumber &&  email &&  licenceNum &&  passportNum && password && addressId) {
       try{
@@ -41,3 +41,26 @@ router.get('/user/:id', (req, res) => {
 module.exports = {
     router: router
 }
+
+
+router.get("/user/delete/:Id", async (req,res) => {
+  const user = await User.query().delete().where({'id': req.params.Id});
+  return res.redirect("/api/user")
+});
+
+module.exports = {
+  router: router
+}
+
+/* 
+{
+         "firstName":"Mike", 
+         "lastName":"Wazowski", 
+         "phoneNumber":"+359886552535",
+         "email":"mike@gmail.com", 
+         "licenceNum" :"1123321",
+         "passportNum":"3332112",
+         "password":"IamCoolMikeWazowski",
+         "addressId":"1"
+}
+*/
